f08c0050e5c35628627fa5fd2fc9995082e5e627
leite <leite@pop-os.localdomain>
2020-09-19 17:41:07 -0300

    funcionando pra RGB e deixando equalization mais generica

--- equalization.py
+++ equalization.py
@@ -1,21 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def equalize(m: np.ndarray) -> np.ndarray:
-    caixas = range(0, 257)
+def equalize(m: np.ndarray, size=256) -> np.ndarray:
+    caixas = range(0, size+1)
     hist, _ = np.histogram(m, caixas)
 
     sum_hist = sum(hist)
-    const = 255/sum_hist
-    saida = np.zeros(256)
-    for i in range(256):
+    const = (size-1)/sum_hist
+    saida = np.zeros(size+1)
+    for i in range(size+1):
         soma = sum(hist[:i+1])
         saida[i] = soma*const
     
     m_eq = np.zeros(m.shape)
     n_lin, n_col = m.shape
+    
     for i in range(n_lin):
         for j in range(n_col):
-            m_eq[i, j] = saida[m[i, j]]
+            m_eq[i, j] = saida[int(m[i, j])]
     
     return m_eq
--- main.py
+++ main.py
@@ -4,18 +4,27 @@ from split_image import split_image
 from equalization import equalize
 from image_merge import image_merge
 
+def rgb2gray(m: np.ndarray, r=0.299, g=0.578, b=0.144) -> np.ndarray:
+    m_gray = r*m[:,:,0] + g*m[:,:,1] + b*m[:,:,2]
+    return np.array(m_gray)
+
 # Load the image
 #file_name = input('Insira o nome da imagem com extenção: ')
 file_name = 'foto.jpg' # REMOVER DEPOIS
 img = plt.imread(file_name)
 
+# Converts to grayscale if the image is RGB
+if len(img.shape) == 3:
+    img = rgb2gray(img)
+
+img = img.astype(int)
 # Ask the number of rows and columns and split the image
 rows = int(input('Insira a quantidade de linhas que a imagem deve ser dividida: '))
 cols = int(input('Insira a quantidade de colunas que a imagem deve ser dividida: '))
 fragment_list = split_image(img, rows, cols)
 
 # For each fragment, equalize it!
-equal_list = [equalize(fragment) for fragment in fragment_list]
+equal_list = [equalize(fragment, size=img.max()) for fragment in fragment_list]
 
 # Join all fragments in a new image
 new_img = image_merge(equal_list, img.shape, rows, cols)
